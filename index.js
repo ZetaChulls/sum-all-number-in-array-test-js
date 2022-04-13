421525fd66cb32ce308bf3afea9f903c8d743058
Charles Ngo <charlesngo37@gmail.com>
2022-04-13 09:41:57 -0700

    Updated to correct ES6 function declaration

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function sumItems(array) {
+const sumItems = (array) => {
   let output = 0;
-  for(const item of array) {
-    if(Array.isArray(item)) {
+  for (const item of array) {
+    if (Array.isArray(item)) {
       output += sumItems(item);
     } else {
       output += item;
     }
   }
   return output;
-}
+};
 
 module.exports = sumItems;
